435b77a8e04c0431437aa05c41a38589f08aee6f
theburi <belika@hotmail.com>
2013-09-17 11:56:07 +0100

    Added xml test parser

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
 <body>
 <?php echo '<h1> Hello World Git</h1>'; ?>
+
+<?php
+	$xmlstr = "<state><temperature>10</temperature></state>";
+	
+	$xmldoc = new SimpleXMLElement($xmlstr);
+	
+	echo "Temperature: " . $xmldoc->temperature;
+ ?>
 </body>
 </html>
