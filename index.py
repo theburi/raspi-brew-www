8c95f8bda40b4d6ccbe4597c067278af16911b17
andrey <belika@hotmail.com>
2013-12-27 06:53:38 +0000

    Sample xml

--- index.py
+++ index.py
@@ -13,54 +13,24 @@ def app(environ, start_response):
     yield '<html><body>'
     yield '<h1>Brewing Master</h1>'
     yield ''
-    # <membership/>
-    membership = Element('membership')
-
-    # <membership><users/>
-    users = SubElement(membership, 'users')
-
-    # <membership><users><user/>
-    SubElement(users, 'user', name='john')
-    SubElement(users, 'user', name='charles')
-    SubElement(users, 'user', name='peter')
-
-    # <membership><groups/>
-    groups = SubElement(membership, 'groups')
-
-    # <membership><groups><group/>
-    group = SubElement(groups, 'group', name='users')
-
-    # <membership><groups><group><user/>
-    SubElement(group, 'user', name='john')
-    SubElement(group, 'user', name='charles')
-
-    # <membership><groups><group/>
-    group = SubElement(groups, 'group', name='administrators')
-
-    # <membership><groups><group><user/>
-    SubElement(group, 'user', name='peter')
     try:
 
-        output_file = open('membership.xml', 'w')
-        output_file.write('<?xml version="1.0"?>')
-        output_file.write(ElementTree.tostring(membership))
-        output_file.close()
+        document = ElementTree.parse('/tmp/brewstate.xml')
+        yield ElementTree.tostring(document)
+        root = document.getroot()
+        for child in root:
+            yield child.tag, child.attrib
+
+        yield '<table>'
+
+        for k, v in sorted(environ.items()):
+            yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
+        yield '</table>'
+        yield '</body></html>'
 
     except:
         yield '<b>%s</b></br>' % sys.exc_info()[0]
 
-    try:
-        document = ElementTree.parse('membership.xml')
-    except :
-        yield '<b>%s</b></br>' %  sys.exc_info()[0]
-
-    yield ElementTree.tostring(document)
-
-    yield '<table>'
-    for k, v in sorted(environ.items()):
-        yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
-    yield '</table>'
-    yield '</body></html>'
 
 
 WSGIServer(app).run()
