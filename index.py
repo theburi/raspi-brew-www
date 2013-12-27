18703cafa04bf909c18abb0062d3cf6b212ec629
andrey <belika@hotmail.com>
2013-12-27 06:16:55 +0000

    Sample xml

--- index.py
+++ index.py
@@ -7,55 +7,58 @@ from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 from xml.etree.ElementTree import SubElement
 
+
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     yield '<html><body>'
     yield '<h1>Brewing Master</h1>'
     yield ''
     # <membership/>
-    membership = Element('membership' )
+    membership = Element('membership')
 
     # <membership><users/>
-    users = SubElement(membership, 'users' )
+    users = SubElement(membership, 'users')
 
     # <membership><users><user/>
-    SubElement(users, 'user', name='john' )
-    SubElement(users, 'user', name='charles' )
-    SubElement(users, 'user', name='peter' )
+    SubElement(users, 'user', name='john')
+    SubElement(users, 'user', name='charles')
+    SubElement(users, 'user', name='peter')
 
     # <membership><groups/>
-    groups = SubElement( membership, 'groups' )
+    groups = SubElement(membership, 'groups')
 
     # <membership><groups><group/>
-    group = SubElement( groups, 'group', name='users' )
+    group = SubElement(groups, 'group', name='users')
 
     # <membership><groups><group><user/>
-    SubElement( group, 'user', name='john' )
-    SubElement( group, 'user', name='charles' )
+    SubElement(group, 'user', name='john')
+    SubElement(group, 'user', name='charles')
 
     # <membership><groups><group/>
-    group = SubElement( groups, 'group', name='administrators' )
+    group = SubElement(groups, 'group', name='administrators')
 
     # <membership><groups><group><user/>
-    SubElement( group, 'user', name='peter' )
-    yield  ElementTree.tostring( membership )
+    SubElement(group, 'user', name='peter')
+    yield ElementTree.tostring(membership)
+    try:
 
-    output_file = open( 'membership.xml', 'w' )
-    output_file.write( '<?xml version="1.0"?>' )
-    output_file.write( ElementTree.tostring( membership ) )
-    output_file.close()
+        output_file = open('membership.xml', 'w')
+        output_file.write('<?xml version="1.0"?>')
+        output_file.write(ElementTree.tostring(membership))
+        output_file.close()
+        break
+    except IOError as e:
+        yield '<b>%s</b></br>' % e
 
-    document = ElementTree.parse( 'membership.xml' )
+    document = ElementTree.parse('membership.xml')
 
     yield ElementTree.tostring(document)
 
-
-
-
     yield '<table>'
     for k, v in sorted(environ.items()):
-         yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
+        yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
     yield '</table>'
     yield '</body></html>'
+
 
 WSGIServer(app).run()
