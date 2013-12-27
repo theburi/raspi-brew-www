05b140958088f50598d29bbc275f0e18c1869bed
andrey <belika@hotmail.com>
2013-12-27 05:50:06 +0000

    Initial sample page

--- index.py
+++ index.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-import web
+from cgi import escape
+import sys, os
+from flup.server.fcgi import WSGIServer
 
-urls = (
-    '/(.*)', 'hello'
-)
-app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'world'
-        return 'Hello, ' + name + '!!!!!!'
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    yield '<html>'
+    yield '<h1>Brewing Master</h1>'
+    yield '<table>'
+    for k, v in sorted(environ.items()):
+         yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
+    yield '</table>'
+    yield '</html>'
 
-if __name__ == "__main__":
-    app.run()
+WSGIServer(app).run()
