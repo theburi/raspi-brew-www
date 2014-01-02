bf9df57a0d44cbd6da29ea25e641dec9dcb5cd2a
andrey <belika@hotmail.com>
2014-01-02 13:16:02 +0000

    Improved State formatting

--- index.py
+++ index.py
@@ -31,8 +31,16 @@ def app(environ, start_response):
         document = ElementTree.parse('/tmp/brewState.xml')
         yield 'reading file'
         root = document.getroot()
-        for child in root:
-            yield '<div> %s - %s </div>' % (child.tag, child.text)
+        yield root[0].tag
+        for step in root.iter('step'):
+            isActive = int(step.attrib['status'])
+            if isActive == 255:
+                yield '<div style=\'color:grey\'> %s - %s </div>' % (step.attrib['name'], step.attrib['status'])
+
+            else:
+                yield '<div style=\'color:black\'> %s - %s </div>' % (step.attrib['name'], step.attrib['status'])
+                yield '<div style=\'margin-left:20px;\'> %s - %s </div>' % (root[1].tag, root[1].text)
+                yield '<div style=\'margin-left:20px;\'> %s - %s </div>' % (root[2].tag, root[2].text)
 
         output_file = ''
         if os.path.exists(ActionPipeName):
@@ -46,10 +54,10 @@ def app(environ, start_response):
 
         yield '<div id=\"log\">Log Entries go here<br></div>'
         yield '</body></html>'
+
     except ParseError as pe:
         yield '<p> %s <p> %s' % (pe.message, pe.text)
     except Exception as e:
-         yield '<p> %s <p> %s' % (e.message, e.text)
-
+        yield '<p> %s' % e.message
 
 WSGIServer(app).run()
